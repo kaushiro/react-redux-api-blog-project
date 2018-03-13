1b00c96eebc131b0c45cd11edba9e0a539d67b13
Kate Howard <kate@kate.com>
2018-03-13 14:41:24 +0000

    Set up initial state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
 
 // import the Router component
 import { BrowserRouter as Router } from "react-router-dom";
@@ -10,27 +11,31 @@ import App from "./App";
 // import in Immutable Map and List for our dummy data
 import { Map, List } from "immutable";
 
+const initial = Map({
+    articles: List([
+        Map({
+            id: 1,
+            title: "Post #1",
+            article: "<p>Blah blah blah</p>",
+            comments: List([
+                Map({ email: "bob@bob.com", comment: "Great blog post" }),
+            ]),
+            tags: List(["blah", "monkeys"]),
+        }),
+        Map({
+            id: 2,
+            title: "Post #2",
+            article: "<p>Blah blah blah</p>",
+            comments: List([
+                Map({ email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" }),
+            ]),
+            tags: List(["blah", "fish-fingers"]),
+        }),
+    ]);
+});
+
 // create some dummy articles
-const articles = List([
-    Map({
-        id: 1,
-        title: "Post #1",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "bob@bob.com", comment: "Great blog post" }),
-        ]),
-        tags: List(["blah", "monkeys"]),
-    }),
-    Map({
-        id: 2,
-        title: "Post #2",
-        article: "<p>Blah blah blah</p>",
-        comments: List([
-            Map({ email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" }),
-        ]),
-        tags: List(["blah", "fish-fingers"]),
-    }),
-]);
+
 
 // wrap the app in Router
 // pass our articles through to App using props
